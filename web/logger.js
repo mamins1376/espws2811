45a652d18984a149e01107ff7a86bead2a18893c
Mohammad Amin Sameti <mamins1376@gmail.com>
2020-12-30 01:13:54 +0330

    Define logger flags as globally usable

--- web/logger.js
+++ web/logger.js
@@ -1,37 +1,38 @@
-const LOGLEVEL_DISABLED = -1;
-const LOGLEVEL_FATAL = 0;
-const LOGLEVEL_ERROR = 1;
+const LOGLEVEL_FATAL   = 0;
+const LOGLEVEL_ERROR   = 1;
 const LOGLEVEL_WARNING = 2;
-const LOGLEVEL_INFO = 3;
-const LOGLEVEL_DEBUG = 4;
+const LOGLEVEL_INFO    = 3;
+const LOGLEVEL_DEBUG   = 4;
 
 const LOGLEVEL = LOGLEVEL_DEBUG;
 
+const LOGLEVEL_FATAL_ENABLED   = LOGLEVEL >= LOGLEVEL_FATAL;
+const LOGLEVEL_ERROR_ENABLED   = LOGLEVEL >= LOGLEVEL_ERROR;
+const LOGLEVEL_WARNING_ENABLED = LOGLEVEL >= LOGLEVEL_WARNING;
+const LOGLEVEL_INFO_ENABLED    = LOGLEVEL >= LOGLEVEL_INFO;
+const LOGLEVEL_DEBUG_ENABLED   = LOGLEVEL >= LOGLEVEL_DEBUG;
+
 export function fatal(...a) {
-  if (LOGLEVEL >= LOGLEVEL_FATAL)
+  if (LOGLEVEL_FATAL_ENABLED)
     console.error("[FATAL]: ", ...a);
 }
 
 export function error(...a) {
-  if (LOGLEVEL >= LOGLEVEL_ERROR)
+  if (LOGLEVEL_ERROR_ENABLED)
     console.error("[ERROR]: ", ...a);
 }
 
 export function warning(...a) {
-  if (LOGLEVEL >= LOGLEVEL_WARNING)
+  if (LOGLEVEL_WARNING_ENABLED)
     console.warn("[WARNING]: ", ...a);
 }
 
 export function info(...a) {
-  if (LOGLEVEL >= LOGLEVEL_INFO)
-  console.log("[INFO]:", ...a);
+  if (LOGLEVEL_INFO_ENABLED)
+    console.log("[INFO]:", ...a);
 }
 
 export function debug(...a) {
-  if (LOGLEVEL >= LOGLEVEL_DEBUG)
-  console.log("[DEBUG]:", ...a);
+  if (LOGLEVEL_DEBUG_ENABLED)
+    console.log("[DEBUG]:", ...a);
 }
-
-Promise.prototype.drive = function() {
-  this.catch(error);
-};
